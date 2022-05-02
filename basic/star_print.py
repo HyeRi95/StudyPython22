fed640c2f7e5913b73dada2091ad77a0319a64ff
HyeRi95 <hyeli95@gmail.com>
2022-05-02 17:50:18 +0900

    220502 기본문법 학습 완료 + 참조 변경

--- basic/star_print.py
+++ basic/star_print.py
@@ -1,7 +1,7 @@
 # *찍기
 for x in range (1,6):
     for y in range(x,6):
-        print('*', end='')
+        print('*', end='') #줄이 바뀌지 않고 그 다음 값이 바로 뒤에 출력된다 
     print('')
 
 print('Hello', end=' ') 
