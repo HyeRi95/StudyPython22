512306e3f87c6d7a16e45475f3624f0b0508b953
chohyeri <hyeli95@gmail.com>
2022-05-13 22:20:00 +0900

    220513 커밋

--- advanced/oracle_handle01.py
+++ advanced/oracle_handle01.py
@@ -11,6 +11,6 @@ query = 'SELECT * FROM memberTBL' # ; 넣으면 안된당
 for row in cur.execute(query):
     print(row)
 
-cur.close() # 네트워크 db 파일 close 꼭꼭!
+cur.close() # 네트워크 db 파일 close 꼭꼭!!
 conn.close()
 
